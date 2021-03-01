6990277ee24f5a6b287c050ea5cec58debab6a7d
M4rukku <57199511+M4rukku@users.noreply.github.com>
2021-03-01 22:21:12 +0100

    Added slight code to controller_interface.py; but generally very simple implementation (interesting if we want to split the program into components)

--- sources/data_controller/controller_interface.py
+++ sources/data_controller/controller_interface.py
@@ -1,9 +1,12 @@
-from sources.frontend.user_queries import UserQueryInformation, UserQueryResponse
 from sources.data_controller.controller import QueryDispatcher
+from sources.frontend.user_queries import UserQueryInformation, \
+    UserQueryResponse
+
 
 class UserQueryHandler:
     def __init__(self):
         self._dispatcher = QueryDispatcher()
 
-    def process_user_query(self, query: UserQueryInformation) -> UserQueryResponse:
-        pass
+    def process_user_query(self,
+                           query: UserQueryInformation) -> UserQueryResponse:
+        return self._dispatcher.process_query(query)
