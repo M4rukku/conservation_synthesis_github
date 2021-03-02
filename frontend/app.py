c0a381ea53f47ee5c9053966b4791a0d193b7e19
tatiana-s <ts790@cam.ac.uk>
2021-03-02 17:42:12 +0100

    Enable post requests for search queries and improve search design

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
 
-#mock up content for the jounrals
+#mock up content for the journals
 def get_journals():
     #read in the frequency value, notice the relative path
     table = pd.read_json("../data/journal_data/journal_usage_frequency.pd.json")
@@ -20,6 +20,18 @@ def index():
 @app.route('/search')
 def search():
     return render_template('search.html',journal_name = get_journals())
+
+@app.route('/handle-query', methods=['POST'])
+def handle_query():
+    if request.form.get('relevant_only'):
+        print("yes relevant")
+    if request.form.get('all_journals'):
+        print("yes journals")
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+    print(start_date)
+    print(end_date)
+    return search()
 
 @app.route('/results')
 def results():
