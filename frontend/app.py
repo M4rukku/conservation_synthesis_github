9da0249813da15c2d9229f84f087d401d0a38c2a
CaoJamie <29327578+CaoJamie@users.noreply.github.com>
2021-03-02 11:17:58 +0800

    implement the initial journal name selection box

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+#mock up content for the jounrals
+def get_journals():
+    #read in the frequency value, notice the relative path
+    table = pd.read_json("../data/journal_data/journal_usage_frequency.pd.json")
+    table.sort_values(by='counts',ascending=False,inplace=True)
+    journal_list = table.index.tolist()
+    #remove empty list
+    journal_list = [x for x in journal_list if x]
+    return journal_list
 
 @app.route('/')
 def index():
@@ -8,7 +19,7 @@ def index():
 
 @app.route('/search')
 def search():
-    return render_template('search.html')
+    return render_template('search.html',journal_name = get_journals())
 
 @app.route('/results')
 def results():
@@ -17,3 +28,7 @@ def results():
 @app.route('/sync')
 def sync():
     return render_template('sync.html')
+
+#run flask under debug mode for development
+if __name__ == '__main__':
+ app.run(debug=True)
