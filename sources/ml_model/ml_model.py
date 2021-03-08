e39b8adfb4a995f3420249824b066fe4119bb2ac
M4rukku <57199511+M4rukku@users.noreply.github.com>
2021-03-08 17:17:29 +0100

    Added Docstrings to MLModelWrapper

--- sources/ml_model/ml_model.py
+++ sources/ml_model/ml_model.py
@@ -1,8 +1,18 @@
 from sources.data_processing.queries import ArticleMetadata
 
 class MlModelWrapper:
+    """The interface that needs to be implemented by the MLModel.
+    """    
     def __init__(self, classifier=None):
         self.classifier = classifier
 
-    def predict_article(self, article: ArticleMetadata):
+    def predict_article(self, article: ArticleMetadata) -> bool:
+        """Predicts the relevance of an article based on its abstract.
+
+        Args:
+            article (ArticleMetadata): The article to be judged.
+
+        Returns:
+            bool: True if relevant, 0 otherwise.
+        """        
         return False
