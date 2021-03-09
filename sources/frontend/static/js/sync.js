10d5dc69f7c913bd22b1147e39a3f135413c7c19
CaoJamie <29327578+CaoJamie@users.noreply.github.com>
2021-03-10 05:20:05 +0800

    implement the jquery to update the element

---
+++ sources/frontend/static/js/sync.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    //connect to the socket server.(assume the document.domain is the same as the server address)
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/sync');
+
+    //receive details from server
+    socket.on('download_update', function(msg) {
+        //debug purpose logging
+        console.log("Received download_update" + msg.download_stats);
+
+        var percentage = msg.download_stats.toString();
+        $("#download").attr("style","width:"+percentage+"%").attr("aria-valuenow",percentage).text(percentage+"%");
+    });
+
+    socket.on('classification_update', function(msg) {
+        //debug purpose logging
+        console.log("Received classification_update" + msg.classification_stats);
+
+        var percentage = msg.classification_stats.toString();
+        $("#classification").attr("style","width:"+percentage+"%").attr("aria-valuenow",percentage).text(percentage+"%");
+    });
+
+    socket.on('finished_update', function(msg) {
+        //debug purpose logging
+        console.log("Received finished_update" + msg.finished_stats);
+        
+        var finished = (msg.finished_stats==='true');
+        $("#finished").text("finished").removeClass("btn-secondary").addClass("btn-info");
+    });
+
+});
