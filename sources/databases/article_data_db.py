295a86cb02ec4de5d9a00680ddd95d66ef5360f1
M4rukku <mw784@cam.ac.uk>
2021-03-09 20:17:20 +0100

    Fixed initial_merge_conflicts

--- sources/databases/article_data_db.py
+++ sources/databases/article_data_db.py
@@ -3,49 +3,6 @@ from sources.databases.internal_databases import InternalSQLDatabase
 from sources.frontend.user_queries import ResultFilter
 
 
-@dataclass
-class DBArticleMetadata:
-    """Class for structuring stored data in the database
-
-    It contains all possible information we can obtain from a query."""
-
-    title: str
-    authors: list
-    doi: str
-    publication_date: str  # date ISO Format yyyy-mm-dd or just yyyy
-    abstract: str
-    repo_identifier: str  # Which repo gave us the data
-    language: str = "EN"
-    publisher: str = None
-    journal_name: str = None
-    journal_volume: str = None
-    journal_issue: str = None
-    issn: str = None
-    url: str = None
-
-    sync_date: str = None
-    checked: bool = None
-
-    classified: str = None
-    relevant: bool = None
-
-    def to_json(self):
-        return json.dumps(asdict(self))
-
-    def as_dict(self):
-        return asdict(self)
-
-    def __repr__(self):
-        returnString = f"""\n \n Title: {self.title} 
-            \n Authors: {", ".join(self.authors)} 
-            \n Published: {self.publication_date} \n DOI: {self.doi}"""
-
-        if self.abstract is not None:
-            returnString += f"""\n Abstract: \n {textwrap.fill(self.abstract,
-                                                               width=80)} \n\n"""
-        return returnString
-
-
 class ArticleRepositoryAPI:
     """ArticleRepositoryAPI defines the interface for interaction with an InternalSQLDatabase.
 
@@ -102,7 +59,6 @@ class ArticleRepositoryAPI:
         result.append(article)
         return result
 
-
     def store_article(self, metadata: DBArticleMetadata):
         """Takes the metadata object and stores it in the internal database.
 
@@ -111,7 +67,7 @@ class ArticleRepositoryAPI:
         """
         self._internal_db.store_article(metadata)
 
-    #Setup Connection
+    # Setup Connection
     def __enter__(self):
         self._internal_db.initialise()
         return self
