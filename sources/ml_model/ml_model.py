8984624c8650c641d775c48d0a1fd45914fe724d
M4rukku <mw784@cam.ac.uk>
2021-03-11 00:24:37 +0100

    Working Deployment scripts

--- sources/ml_model/ml_model.py
+++ sources/ml_model/ml_model.py
@@ -1,10 +1,13 @@
 from sources.data_processing.queries import ArticleMetadata
+from sources.ml_model.pytorch_model import PytorchModel
+
 
 class MlModelWrapper:
     """The interface that needs to be implemented by the MLModel.
     """    
     def __init__(self, classifier=None):
-        self.classifier = classifier
+        self.classifier = PytorchModel()
+
 
     def predict_article(self, article: ArticleMetadata) -> bool:
         """Predicts the relevance of an article based on its abstract.
@@ -14,5 +17,6 @@ class MlModelWrapper:
 
         Returns:
             bool: True if relevant, 0 otherwise.
-        """        
+        """
         return False
+        #return self.classifier.do_prediction(article.title, article.journal_name, article.abstract)
---
+++ sources/ml_model/pytorch_model.py
@@ -0,0 +1,7 @@
+class PytorchModel:
+    def __init__(self):
+        #setup codde here
+        pass
+
+    def do_prediction(self, title, journal_name, abstract):
+        pass
