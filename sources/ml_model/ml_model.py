8679eb308d623a0eebead2b614b448fed509cb91
M4rukku <mw784@cam.ac.uk>
2021-03-11 12:27:44 +0100

    Reverted prior changes

--- sources/ml_model/ml_model.py
+++ sources/ml_model/ml_model.py
@@ -1,12 +1,14 @@
 from sources.data_processing.queries import ArticleMetadata
-from sources.ml_model.pytorch_model import PytorchModel
 
 
 class MlModelWrapper:
     """The interface that needs to be implemented by the MLModel.
+
+    CURRENTLY having problems with integration! So dummy classifier is used.
     """    
     def __init__(self, classifier=None):
-        self.classifier = PytorchModel()
+        #self.classifier = PytorchModel()
+        pass
 
 
     def predict_article(self, article: ArticleMetadata) -> bool:
@@ -18,4 +20,5 @@ class MlModelWrapper:
         Returns:
             bool: True if relevant, False otherwise.
         """
-        return self.classifier.do_prediction(article.title, article.journal_name, article.abstract)
+        return False
+        #return self.classifier.do_prediction(article.title, article.journal_name, article.abstract)
